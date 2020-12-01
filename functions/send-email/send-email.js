71cc2a848244ca2d8d44c269ad7fdb4275d3128b
gandroide <ajontiveros82@gmail.com>
2020-12-01 23:45:11 +0000

    cambio a correos detinatarios de empresa

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -59,7 +59,7 @@ const handler = async (event) => {
 
   let info = await transporter.sendMail({
     from: '"Electro Flow" <relatorio.electroflow@gmail.com>', // sender address
-    to: "ajontiveros82@gmail.com,", // list of receivers
+    to: "administrativo@electroflow.com,flavio.figueira@electroflow.com", // list of receivers
     subject: `Data: ${fecha} Tecnico: ${tecnico} Cliente: ${cliente}`, // Subject line 
     html: `
     <!doctype html>
